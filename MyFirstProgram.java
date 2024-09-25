6c2379401d1179a8fe33dd37dacc53e242c1c328
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-25 13:53:51 +0100

    added myfirst program ( but is the same as previous)

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -5,12 +5,12 @@
 */
 
 //class definition
-public class MyFirstProgram{ 
-	//mispelling
+public class MyFirstProgram{
 	//main method header
-	public static void main(String args[]){ // missing the closed [], main was capitalize
-		System.out.println("I love Maths"); //System not capitalize
-		System.out.println("And I love java too"); // println misspel , no double quote / missing ;
+	public static void main(String args[]){
+		System.out.println("I love Maths");
+		System.out.println("And I love java too");
+
 	}
 }
-// extra braquets
+
