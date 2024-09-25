9921e336c7cae2e684549cc2de3443f553fbcd04
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-25 13:29:11 +0100

    Create SimpleAddition.java
    file added, and fixed as part of class 1

---
+++ SimpleAddition.java
@@ -0,0 +1,11 @@
+public class SimpleAddition{
+	public static void main(String[] args){
+		/*Fill in the gaps in code by replacing the **** sections*/
+		//addition
+		int x=7; // missing the variable (x) and the ;
+		int y=8;
+		int ans=x+y; // missing variable (y)
+		//print
+		System.out.println(ans); //missing println expression and variable (ans) 
+	}
+}
