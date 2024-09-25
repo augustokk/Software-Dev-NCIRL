29a5333320741a3fd5275c9bfe412048e4b2d943
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-25 13:48:47 +0100

    Lab 1 initials has been added
    Lab 1 initials has been added

---
+++ Lab1Initials.java
@@ -0,0 +1,10 @@
+public class Lab1Initials {
+    public static void main(String[] args) {
+        // Printing the letters J and P side by side
+        System.out.println("   JJJJJJJ  PPPPPP");
+        System.out.println("      J     P    P");
+        System.out.println("      J     PPPPPP");
+        System.out.println("      J     P");
+        System.out.println("    JJJ     P");
+    }
+}
