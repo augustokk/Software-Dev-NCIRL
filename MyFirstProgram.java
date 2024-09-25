31e5569048fc073fe082e41d641ff388a12c0d07
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-25 13:21:26 +0100

    Create MyFirstProgram.java

---
+++ MyFirstProgram.java
@@ -0,0 +1,16 @@
+/*
+*MyFirstProgram.java
+*@author S.Cogan
+*
+*/
+
+//class definition
+public class MyFirstProgram{ 
+	//mispelling
+	//main method header
+	public static void main(String args[]){ // missing the closed [], main was capitalize
+		System.out.println("I love Maths"); //System not capitalize
+		System.out.println("And I love java too"); // println misspel , no double quote / missing ;
+	}
+}
+// extra braquets
