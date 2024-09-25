b018368bb304286bb83e4c12ba48970668c92f6a
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-25 14:05:36 +0100

    fixme file added and fixed

---
+++ fixme.java
@@ -0,0 +1,13 @@
+public class fixme{ // add word class
+	public static void main(String[] args){ // String starts with capitalize
+		/*Fill in the gaps in code by replacing the **** sections
+		//There may be errors other than just blanks, but start with blanks first
+		*/
+		//addition
+		int x=7; // add int and add ;
+		int y=8; // change String to int
+		int ans=x + y; // its not a multiplication it is a addition
+		//print
+		System.out.println(ans); // variable ans needs to be out of quotes
+	}
+} // missing brackets
