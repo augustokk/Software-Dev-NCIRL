b5dc44e209f1a84fadfb3f8fa0dc75c38d3f3af6
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-28 21:38:45 +0100

    create a operator file

---
+++ AULA 2 Java/Operators.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String args[])
+    {
+        // Declare variables (when dealing with numbers and operators, pay attention to define the variable as int or double, because the result of the operation will be based on that, you can mix them and double will be the main result)
+
+        int num1= 20;
+        double num2= 3.0;
+
+        //output
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    
+    }
+}
--- AULA 2 Java/Variables.java
+++ AULA 2 Java/Variables.java
@@ -1,19 +1,12 @@
 public class Variables {
     public static void main(String args[])
     {
-        // Declare variables
-        int num1;
-        double num2;
-        String name;
-        char initial;
-        boolean check;
-
-        // Initialize variables
-        num1 = 7;
-        num2 = 3.5;
-        name = "Augusto";
-        initial = 'A';
-        check = true;
+        // Declare variables and Initialize variables
+        int num1 = 7;
+        double num2 = 3.5;
+        String name = "Augusto";
+        char initial = 'A';
+        boolean check = true;
 
         // Print variables
         System.out.println(num1);
