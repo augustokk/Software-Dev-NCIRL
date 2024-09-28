83f7a4c662b1eb2ea61aff7a43a42d4075b2d900
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-28 21:19:23 +0100

    Started class 2 creating a variable file
    also separated the lessons in different folders

---
+++ AULA 2 Java/Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String args[])
+    {
+        // Declare variables
+        int num1;
+        double num2;
+        String name;
+        char initial;
+        boolean check;
+
+        // Initialize variables
+        num1 = 7;
+        num2 = 3.5;
+        name = "Augusto";
+        initial = 'A';
+        check = true;
+
+        // Print variables
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(name);
+        System.out.println(initial);
+        System.out.println(check);
+    }
+}
