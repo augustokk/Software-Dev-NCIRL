1b32b8f0077f7422dc0c6020ed6ca72f069f0987
Augusto Kuusberg <53272493+augustokk@users.noreply.github.com>
2024-09-29 18:30:23 +0100

    exercise lab done

---
+++ AULA 3 Java/Class Materials-Week3/EggBoxApp/EggBox.java
@@ -0,0 +1,29 @@
+//fix the code by figuring out what should be in the **** sections
+public class EggBox{
+	//vars
+	**** int eggs;
+	**** **** boxSize;
+	**** **** numBoxes;
+	**** **** leftOverEggs;
+	//set
+	public **** setEggs(int eggs){
+		this.eggs=eggs;
+	}
+	public **** setBoxSize(int ****){
+		this.****=****;
+	}
+	//compute
+	public void computeBoxes(){
+		numBoxes=eggs/6;
+	}
+	public void computeLeftover(){
+		leftOverEggs=****;
+	}
+	//get
+	public **** getBoxes(){
+		return boxes;
+	}
+	public **** getLeftover(){
+		return leftover;
+	}
+}
---
+++ AULA 3 Java/Class Materials-Week3/EggBoxApp/EggBoxApp.java
@@ -0,0 +1,29 @@
+//fix the code by figuring out what should be in the **** sections
+import javax.****.JOptionPane;
+public class EggBox****{
+	public static void main(String[] args){
+		//variables
+		int eggs;
+		int boxSize;
+		int numBoxes;
+		int leftOverEggs;
+		//objects
+		**** egg;
+		****=new EggBox();
+		//inputs
+		*****=Integer.****(JOptionPane.****(null,"Enter a number of eggs"));
+		boxSize=6;
+		//set
+		egg.setEggs(****);
+		egg.setBoxSize(****);
+		//compute
+		****.computeBoxes();
+		****.computeLeftover();
+		//get
+		****=egg.getBoxes();
+		****=egg.getLeftover();
+		//output
+		JOptionPane.****(null,"Number of boxes needed "+numBoxes);
+		JOptionPane.****(null,"Number of eggs left over "+leftOverEggs);
+	}
+}
